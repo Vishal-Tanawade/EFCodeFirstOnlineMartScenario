dcb33037dfe07aa2c5bda4f55f78b9aa015ccbf8
vishaltanawade688@gmail.com <vishaltanawade688@gmail.com>
2022-05-25 15:49:24 +0530

    migration added

---
+++ EFCodeFirstOnlineMartScenario/Migrations/202205251019191_initialization.cs
@@ -0,0 +1,94 @@
+﻿namespace EFCodeFirstOnlineMartScenario.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class initialization : DbMigration
+    {
+        public override void Up()
+        {
+            CreateTable(
+                "dbo.Carts",
+                c => new
+                    {
+                        CartId = c.Int(nullable: false, identity: true),
+                        CustomerId = c.Int(nullable: false),
+                        ProductID = c.Int(nullable: false),
+                        ProductPrice = c.Decimal(nullable: false, precision: 18, scale: 2),
+                        ProductQTY = c.Int(nullable: false),
+                        TotalAmout = c.Decimal(nullable: false, precision: 18, scale: 2),
+                    })
+                .PrimaryKey(t => t.CartId)
+                .ForeignKey("dbo.Customers", t => t.CustomerId, cascadeDelete: true)
+                .ForeignKey("dbo.Products", t => t.ProductID, cascadeDelete: true)
+                .Index(t => t.CustomerId)
+                .Index(t => t.ProductID);
+            
+            CreateTable(
+                "dbo.Customers",
+                c => new
+                    {
+                        CustomerId = c.Int(nullable: false, identity: true),
+                        CustomerName = c.String(nullable: false, maxLength: 30),
+                        CustomerEmail = c.String(nullable: false),
+                        CustomerContact = c.String(nullable: false),
+                    })
+                .PrimaryKey(t => t.CustomerId);
+            
+            CreateTable(
+                "dbo.Products",
+                c => new
+                    {
+                        ProductID = c.Int(nullable: false, identity: true),
+                        SupplierId = c.Int(nullable: false),
+                        CategoryId = c.Int(nullable: false),
+                        ProductName = c.String(nullable: false),
+                        ProductPrice = c.Decimal(nullable: false, precision: 18, scale: 2),
+                        ProductQty = c.Int(nullable: false),
+                    })
+                .PrimaryKey(t => t.ProductID)
+                .ForeignKey("dbo.Categories", t => t.CategoryId, cascadeDelete: true)
+                .ForeignKey("dbo.Suppliers", t => t.SupplierId, cascadeDelete: true)
+                .Index(t => t.SupplierId)
+                .Index(t => t.CategoryId);
+            
+            CreateTable(
+                "dbo.Categories",
+                c => new
+                    {
+                        CategoryId = c.Int(nullable: false, identity: true),
+                        CategoryType = c.String(nullable: false),
+                    })
+                .PrimaryKey(t => t.CategoryId);
+            
+            CreateTable(
+                "dbo.Suppliers",
+                c => new
+                    {
+                        SupplierId = c.Int(nullable: false, identity: true),
+                        SupplierName = c.String(nullable: false, maxLength: 30),
+                        SupplierContact = c.String(nullable: false),
+                        SupplierCity = c.String(nullable: false),
+                    })
+                .PrimaryKey(t => t.SupplierId);
+            
+        }
+        
+        public override void Down()
+        {
+            DropForeignKey("dbo.Carts", "ProductID", "dbo.Products");
+            DropForeignKey("dbo.Products", "SupplierId", "dbo.Suppliers");
+            DropForeignKey("dbo.Products", "CategoryId", "dbo.Categories");
+            DropForeignKey("dbo.Carts", "CustomerId", "dbo.Customers");
+            DropIndex("dbo.Products", new[] { "CategoryId" });
+            DropIndex("dbo.Products", new[] { "SupplierId" });
+            DropIndex("dbo.Carts", new[] { "ProductID" });
+            DropIndex("dbo.Carts", new[] { "CustomerId" });
+            DropTable("dbo.Suppliers");
+            DropTable("dbo.Categories");
+            DropTable("dbo.Products");
+            DropTable("dbo.Customers");
+            DropTable("dbo.Carts");
+        }
+    }
+}
